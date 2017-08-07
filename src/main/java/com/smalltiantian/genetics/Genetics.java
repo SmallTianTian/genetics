00fe935abe98604fb8ff65a79a0f09d9a91fa587
tianxuxin <tianxuxin@qiniu.com>
2017-08-07 11:16:06 +0800

    perfect Genetics.init

--- src/main/java/com/smalltiantian/genetics/Genetics.java
+++ src/main/java/com/smalltiantian/genetics/Genetics.java
@@ -1,24 +1,44 @@
 package com.smalltiantian.genetics;
 
+import java.io.File;
+import java.io.FileInputStream;
+import org.apache.commons.io.IOUtils;
+import com.google.gson.JsonObject;
+
 public class Genetics {
     private static Genetics genetics = null;
 
     public static Genetics init() {
-        return init(null);
+        if (genetics != null)
+            throw new IllegalStateException("Error : You already init this(`Genetics`).");
+        Config config = Config.initByDefault();
+        genetics = new Genetics();
+        return genetics;
     }
 
     public static Genetics init(String configPath) {
         if (genetics != null)
             throw new IllegalStateException("Error : You already init this(`Genetics`).");
-        if (configPath != null) {
-            
-        } else {
-            
+        FileInputStream is = null;
+        try {
+            is = new FileInputStream(new File(configPath));
+            String configStr = IOUtils.toString(is);
+            JsonObject obj   = BaseUtil.gson().fromJson(configStr, JsonObject.class);
+            String saveAddress = obj.get("saveAddress").getAsString();
+            int varianceRatio  = obj.get("varianceRatio").getAsInt();
+            int saveTimer      = obj.get("saveTimer").getAsInt();
+            int countThreadNum = obj.get("countThreadNum").getAsInt();
+            Config config = Config.init(saveAddress, varianceRatio, saveTimer, countThreadNum);
+            genetics = new Genetics();
+        } catch (Exception e) {
+            throw new RuntimeException("init Failed", e);
+        } finally {
+            IOUtils.closeQuietly(is);
         }
         return genetics;
     }
 
-    public void begin() {
+    public void beginGenetics() {
         
     }
 }
