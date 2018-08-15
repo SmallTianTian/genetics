e0c100b47a8556c2c2db3adf262cd3dd9d3d51a8
tianxuxin <tianxuxin@qiniu.com>
2018-08-15 16:53:13 +0800

    remove BaseTest and refactor BaseDataHelper.

---
+++ src/test/java/com/smalltiantian/genetics/test/TestHelper.java
@@ -0,0 +1,11 @@
+package com.smalltiantian.genetics;
+
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class TestHelper {
+    public static Earth produceEarth(Map<String, String> map) {
+        return null;
+    }
+}
